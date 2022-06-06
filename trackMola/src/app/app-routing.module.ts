56887beb88b2ed4be6abb8273298b50f86de7af8
andrei4ik1997 <andrey03071997@yandex.ru>
2022-06-06 23:34:22 +0300

    feat: add routing

--- trackMola/src/app/app-routing.module.ts
+++ trackMola/src/app/app-routing.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: LoginPageComponent },
+  {
+    path: 'employee',
+    loadChildren: () =>
+      import('./employee/employee.module').then((x) => x.EmployeeModule),
+  },
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [
+    RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules }),
+  ],
   exports: [RouterModule],
 })
 export class AppRoutingModule {}
