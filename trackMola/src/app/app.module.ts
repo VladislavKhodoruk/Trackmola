939e47e8c4656b1da8eeeb628a17187deca9f21e
andrei4ik1997 <andrey03071997@yandex.ru>
2022-06-06 23:37:48 +0300

    fix: some changes

--- trackMola/src/app/app.module.ts
+++ trackMola/src/app/app.module.ts
@@ -4,17 +4,28 @@ import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreRouterConnectingModule } from '@ngrx/router-store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { AngularFireModule } from '@angular/fire/compat';
+import { AngularFireAuthModule } from '@angular/fire/compat/auth';
+import { AngularFireStorageModule } from '@angular/fire/compat/storage';
+import { AngularFirestoreModule } from '@angular/fire/compat/firestore';
+import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
 import { environment } from 'src/environments/environment';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { TrackMolaEffects } from './store/trackMola.effects';
 import { reducer } from './store/trackMola.reducer';
+import { LoginPageComponent } from './login-page/login-page.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from './common/shared.module';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, LoginPageComponent],
   imports: [
+    SharedModule,
     BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
     AppRoutingModule,
     StoreModule.forRoot({ trackMola: reducer }),
     StoreDevtoolsModule.instrument({
@@ -23,6 +34,11 @@ import { reducer } from './store/trackMola.reducer';
     }),
     EffectsModule.forRoot([TrackMolaEffects]),
     StoreRouterConnectingModule.forRoot(),
+    AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFireAuthModule,
+    AngularFirestoreModule,
+    AngularFireStorageModule,
+    AngularFireDatabaseModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
