c7d1085e1736e8b8d4500271664468b89ed54837
andrei4ik1997 <andrey03071997@yandex.ru>
2022-06-12 23:45:08 +0300

    fix: add employee state

---
+++ trackMola/src/app/pages/employee/store/employee.selectors.ts
@@ -0,0 +1,13 @@
+import { createFeatureSelector, createSelector } from '@ngrx/store';
+import { ProfileUser } from 'src/app/shared/interfaces';
+import { EmployeeState } from './employee.state';
+
+export const EMPLOYEE_STATE_NAME = 'employee';
+
+const getEmployeeState =
+  createFeatureSelector<EmployeeState>(EMPLOYEE_STATE_NAME);
+
+export const getUserPhoto = createSelector(getEmployeeState, (state) => {
+  const usetInfo: ProfileUser | null = state.user;
+  return usetInfo?.photo;
+});
