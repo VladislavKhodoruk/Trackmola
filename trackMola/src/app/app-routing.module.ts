2d1a18610bc96ce59a6e87aae6ee340d96ab7c13
andrei4ik1997 <andrey03071997@yandex.ru>
2022-06-12 23:41:44 +0300

    fix: add guard

--- trackMola/src/app/app-routing.module.ts
+++ trackMola/src/app/app-routing.module.ts
@@ -1,20 +1,23 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { AuthrorizationGuard } from './pages/authrorization/authrorization.guard';
 
 const routes: Routes = [
   { path: '', redirectTo: 'authrorization', pathMatch: 'full' },
   {
     path: 'employee',
     loadChildren: () =>
-      import('./employee/employee.module').then((x) => x.EmployeeModule),
+      import('./pages/employee/employee.module').then((x) => x.EmployeeModule),
+    canActivate: [AuthrorizationGuard],
   },
   {
     path: 'authrorization',
     loadChildren: () =>
-      import('./authrorization/authrorization.module').then(
+      import('./pages/authrorization/authrorization.module').then(
         (x) => x.AuthorizationModule
       ),
   },
+  { path: '**', redirectTo: 'authrorization' },
 ];
 
 @NgModule({
---
+++ trackMola/src/app/pages/authrorization/authrorization.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { CanActivate, Router, UrlTree } from '@angular/router';
+import { map, Observable, take, tap } from 'rxjs';
+import { AuthorizationService } from './services/authorization.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthrorizationGuard implements CanActivate {
+  constructor(
+    private authorization: AuthorizationService,
+    private router: Router
+  ) {}
+  canActivate():
+    | Observable<boolean | UrlTree>
+    | Promise<boolean | UrlTree>
+    | boolean
+    | UrlTree {
+    return this.authorization.currentUser$.pipe(
+      take(1),
+      map((currentUser) => !!currentUser),
+      tap((isAuthorized) => {
+        if (!isAuthorized) {
+          this.router.navigate(['']);
+        }
+      })
+    );
+  }
+}
