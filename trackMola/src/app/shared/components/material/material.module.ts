20da446db04b4be2eb150670dac2809fce443dda
andrei4ik1997 <andrey03071997@yandex.ru>
2022-06-21 21:30:59 +0300

    feature: integrate angular material 14

---
+++ trackMola/src/app/shared/components/material/material.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatAutocompleteModule } from '@angular/material/autocomplete';
+
+@NgModule({
+  declarations: [],
+  imports: [],
+  exports: [
+    MatTableModule,
+    MatIconModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatAutocompleteModule,
+  ],
+})
+export class MaterialModule {}
