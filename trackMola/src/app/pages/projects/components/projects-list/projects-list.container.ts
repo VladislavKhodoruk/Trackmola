1da4db750fb1f5cfdf7eaf4c1c7cb545ad41c6f6
andrei4ik1997 <andrey03071997@yandex.ru>
2022-06-24 00:02:46 +0300

    feature: create project list component

---
+++ trackMola/src/app/pages/projects/components/projects-list/projects-list.container.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-projects-list-container',
+  template: '<app-projects-list></app-projects-list>',
+})
+export class ProjectsListContainer implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
