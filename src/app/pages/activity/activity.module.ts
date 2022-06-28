3ca6f02ee93985e115db7dac225473f413e508ce
Andrei Shynkarou <ashinkarev@datamola.com>
2022-06-28 17:44:04 +0300

    feature: install angular-highcharts

--- src/app/pages/activity/activity.module.ts
+++ src/app/pages/activity/activity.module.ts
@@ -12,6 +12,8 @@ import { AdminActivityComponent } from './components/admin-activity/admin-activi
 import { ManagerActivityComponent } from './components/manager-activity/manager-activity.component';
 import { ActivityContainer } from './layout/activity.container';
 import { ActivityComponent } from './layout/activity.component';
+import { ProjectsActivityComponent } from './components/projects-activity/projects-activity.component';
+import { ChartModule } from 'angular-highcharts';
 
 @NgModule({
   declarations: [
@@ -21,9 +23,11 @@ import { ActivityComponent } from './layout/activity.component';
     ManagerActivityComponent,
     ActivityContainer,
     ActivityComponent,
+    ProjectsActivityComponent,
   ],
   imports: [
     CommonModule,
+    ChartModule,
     EffectsModule.forFeature([]),
     StoreModule.forFeature(ACTIVITY_STATE_NAME, ActivityReducer),
     RouterModule.forChild(ActivityRoutes),
