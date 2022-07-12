6e3c8a309c4dfe753d2ac02e695686e3d25c8684
Vitaly Cheastyakov <vchistiakov@datamola.com>
2022-07-12 15:32:49 +0300

    fix name of output

--- src/app/pages/activity/components/employee-activity/employee-activity.component.ts
+++ src/app/pages/activity/components/employee-activity/employee-activity.component.ts
@@ -8,7 +8,7 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 export class EmployeeActivityComponent {
   @Input() period: string;
 
-  @Output() selectedPeriod = new EventEmitter<string>();
+  @Output() selectPeriod = new EventEmitter<string>();
 
   labels = ['Week', 'Month', 'Year'];
 }
