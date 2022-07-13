40dd68305e1268211199fb3582aa564323c33b18
mivakhnenko <mivakhnenko@datamola.com>
2022-07-13 12:43:20 +0300

    fix:add fonts

--- src/app/pages/projects/components/projects-team/projects-team.component.ts
+++ src/app/pages/projects/components/projects-team/projects-team.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { Project, TaskTrack, User } from '@shared/interfaces/interfaces';
 
 @Component({
