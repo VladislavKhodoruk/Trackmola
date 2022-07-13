8e70e23bd49dc6ec7c0adba66ed416fdbc8d2755
Andrei Shinkarev <ashinkarev@datamola.com>
2022-07-13 21:18:31 +0300

    feature: add clear project state action

--- src/app/pages/projects/layout/projects.container.ts
+++ src/app/pages/projects/layout/projects.container.ts
@@ -1,7 +1,9 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnDestroy } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { Observable } from 'rxjs';
+
+import { clearProjectState } from '../store/projects.actions';
 
 import { getProjectByRoute } from '@pages/projects/store/projects.selectors';
 import { Project } from '@shared/interfaces/interfaces';
@@ -16,9 +18,13 @@ import { TrackMolaState } from '@store/trackMola.state';
   styleUrls: ['./projects.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class ProjectsContainer {
+export class ProjectsContainer implements OnDestroy {
   readonly projectByRoute$: Observable<Project> =
     this.store$.select(getProjectByRoute);
 
   constructor(private store$: Store<TrackMolaState>) {}
+
+  ngOnDestroy(): void {
+    this.store$.dispatch(clearProjectState());
+  }
 }
