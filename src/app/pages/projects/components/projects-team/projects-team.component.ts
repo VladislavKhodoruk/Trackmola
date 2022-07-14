e164293a760873cf87bfa8628fbfe2015137655a
mivakhnenko <mivakhnenko@datamola.com>
2022-07-14 20:02:37 +0300

    fix:fix comments + imports

--- src/app/pages/projects/components/projects-team/projects-team.component.ts
+++ src/app/pages/projects/components/projects-team/projects-team.component.ts
@@ -1,10 +1,8 @@
 import { Component, Input } from '@angular/core';
-import { Store } from '@ngrx/store';
 
 import { DEFAULT_PHOTO_URL } from '@shared/constants/constants';
-
 import { GroupBy, Project, User } from '@shared/interfaces/interfaces';
-import { TrackMolaState } from '@store/trackMola.state';
+
 @Component({
   selector: 'app-projects-team',
   templateUrl: './projects-team.component.html',
@@ -15,6 +13,4 @@ export class ProjectsTeamComponent {
   @Input() readonly usersGroupByProject: GroupBy<User[]>;
 
   readonly defaultPhoto: string = DEFAULT_PHOTO_URL;
-
-  constructor(private store$: Store<TrackMolaState>) {}
 }
