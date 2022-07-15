2edebba9d13471c4e0cc730813d8ad2051eafc57
Vladislav Khodoruk <vkhodoruk@datamola.com>
2022-07-15 22:34:34 +0300

    fix: lint

--- src/app/pages/dashboard/dashboard.module.ts
+++ src/app/pages/dashboard/dashboard.module.ts
@@ -19,9 +19,9 @@ import { DashboardReducer } from './store/dashboard.reducer';
 import { DASHBOARD_STATE_NAME } from './store/dashboard.selectors';
 
 import { ActiveTasksListModule } from '@pages/dashboard/components/active-tasks-list/active-tasks-list-module';
-import { CalendarModule } from '@shared/components/calendar/calendar.module';
-import { ActivityWeekChartContainer } from '@pages/dashboard/components/activity-week-chart/activity-week-chart.container';
 import { ActivityWeekChartComponent } from '@pages/dashboard/components/activity-week-chart/activity-week-chart.component';
+import { ActivityWeekChartContainer } from '@pages/dashboard/components/activity-week-chart/activity-week-chart.container';
+import { CalendarModule } from '@shared/components/calendar/calendar.module';
 import { HighchartsChartModule } from '@shared/components/highcharts-chart/highcharts-chart.module';
 
 @NgModule({
