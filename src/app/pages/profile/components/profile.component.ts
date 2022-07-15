eaca1642921271a1adbd1e13758dbae5efa6d2e2
mivakhnenko <mivakhnenko@datamola.com>
2022-07-15 23:51:51 +0300

    fix:fix default photo

--- src/app/pages/profile/components/profile.component.ts
+++ src/app/pages/profile/components/profile.component.ts
@@ -1,5 +1,7 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 
+import { DEFAULT_PHOTO_URL } from '@shared/constants/constants';
+
 import { User } from '@shared/interfaces/interfaces';
 
 @Component({
@@ -11,6 +13,8 @@ export class ProfileComponent {
   @Input() userInfo: User;
 
   @Output() logoutEmmiter = new EventEmitter<void>();
+
+  readonly defaultPhoto: string = DEFAULT_PHOTO_URL;
 
   logout(event: Event): void {
     event.preventDefault();
