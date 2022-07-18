128b06d33307f0f6e0fd2dabd9a4855779452c5a
Andrei Shinkarev <ashinkarev@datamola.com>
2022-07-18 21:01:34 +0300

    modify: vacations component

--- src/app/pages/projects/layout/projects.component.ts
+++ src/app/pages/projects/layout/projects.component.ts
@@ -1,7 +1,12 @@
 import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import angleRight from '@iconify/icons-uil/angle-right';
 
-import { GroupBy, Project, User } from '@shared/interfaces/interfaces';
+import {
+  GroupBy,
+  Project,
+  User,
+  Vacations,
+} from '@shared/interfaces/interfaces';
 
 @Component({
   selector: 'app-projects',
@@ -14,4 +19,33 @@ export class ProjectsComponent {
   @Input() readonly usersGroupByProject: GroupBy<User[]>;
 
   readonly iconAngleRight = angleRight;
+
+  readonly vacations: Vacations[] = [
+    {
+      vacationDay: new Date('2022-07-22T03:24:00'),
+      fullName: 'Kirill Borisenok',
+      photo:
+        'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net' +
+        '/629860aa1a2bdf007095b9bb/181bf52b-1854-4deb-a661-884a6a376802/128',
+    },
+    {
+      vacationDay: new Date('2022-07-23T03:24:00'),
+      fullName: 'Andrei Shinkarev',
+      photo:
+        'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net' +
+        '/62985f0ac3dffc0068f5ddff/864cad92-da20-48f3-b5b4-54d2a6c74170/128',
+    },
+    {
+      vacationDay: new Date('2022-07-24T03:24:00'),
+      fullName: 'Vladislav Khodoruk',
+      photo:
+        'https://avatar-management--avatars.us-west-2.prod.public.atl-paas.net' +
+        '/6298604da0eafd0069e8f7cd/1656ba5b-b78b-4822-846f-ecb40c4e4899/128',
+    },
+    {
+      vacationDay: new Date('2022-07-25T03:24:00'),
+      fullName: 'Maria Ivakhnenko',
+      photo: 'https://avatars.githubusercontent.com/u/88663763?v=4',
+    },
+  ];
 }
