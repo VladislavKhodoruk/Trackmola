fc4b5edc8068c1137591a5e5b99c761b4dc3ecbe
Vitaly Cheastyakov <vchistiakov@datamola.com>
2022-07-18 10:43:06 +0300

    fix tags

--- src/app/pages/dashboard/components/activity-week-chart/activity-week-chart.container.ts
+++ src/app/pages/dashboard/components/activity-week-chart/activity-week-chart.container.ts
@@ -2,7 +2,8 @@ import { Component, ChangeDetectionStrategy } from '@angular/core';
 
 @Component({
   selector: 'app-activity-week-chart-container',
-  template: `<app-activity-week-chart-component></app-activity-week-chart-component>`,
+  template:
+    '<app-activity-week-chart-component></app-activity-week-chart-component>',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class ActivityWeekChartContainer {}
