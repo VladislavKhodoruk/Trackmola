5bb9646d242c124ced4b72bcd8f3a003d40ee413
Andrei Shinkarev <ashinkarev@datamola.com>
2022-07-21 23:38:33 +0300

    feature: add modal

--- src/app/shared/components/report-input/report-input.component.ts
+++ src/app/shared/components/report-input/report-input.component.ts
@@ -163,6 +163,9 @@ export class ReportInputComponent implements OnInit, OnChanges {
         curTaskTrack.taskId === curTaskId &&
         curTaskTrack.userId === localStorage.getItem('AuthUserId')
     );
+    if (!usersTaskTracks.length) {
+      return +DurationValue.Default;
+    }
     const averageDuration =
       usersTaskTracks.reduce((acc, cur) => (acc += cur.duration), 0) /
       usersTaskTracks.length;
