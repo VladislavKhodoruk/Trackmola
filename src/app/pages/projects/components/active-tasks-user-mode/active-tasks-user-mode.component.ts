809bf1085c35e1b17c65779ca15aebadaf9a0c5d
Kirill Borisenok <kborisenok@datamola.com>
2022-07-24 14:37:54 +0300

    feature: created active tasks user mode on the projects page

---
+++ src/app/pages/projects/components/active-tasks-user-mode/active-tasks-user-mode.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+
+import { getRandomColor } from '@shared/helpers/helpers';
+
+import { GroupBy, Project, Task } from '@shared/interfaces/interfaces';
+
+@Component({
+  selector: 'app-active-tasks-user-mode',
+  templateUrl: './active-tasks-user-mode.component.html',
+  styleUrls: ['./active-tasks-user-mode.component.scss'],
+})
+export class ActiveTasksUserModeComponent {
+  @Input() readonly project: Project;
+  @Input() readonly activeTaskGroupByProject: GroupBy<Task[]>;
+  @Input() readonly activeTaskTracksDurationGroupByTask: GroupBy<number>;
+  @Input() readonly tasksInfoByTaskId: GroupBy<Task>;
+
+  getRandomColor(): string {
+    return getRandomColor();
+  }
+}
