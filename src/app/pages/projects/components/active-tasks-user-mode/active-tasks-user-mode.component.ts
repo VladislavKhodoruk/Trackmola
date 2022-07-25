5c73c25277334db58322ae1eddf788fe857074b9
Andrei Shinkarev <ashinkarev@datamola.com>
2022-07-25 17:54:32 +0300

    fix: merge

--- src/app/pages/projects/components/active-tasks-user-mode/active-tasks-user-mode.component.ts
+++ src/app/pages/projects/components/active-tasks-user-mode/active-tasks-user-mode.component.ts
@@ -6,8 +6,8 @@ import { GroupBy, Project, Task } from '@shared/interfaces/interfaces';
 
 @Component({
   selector: 'app-active-tasks-user-mode',
-  templateUrl: './active-tasks-user-mode.component.html',
   styleUrls: ['./active-tasks-user-mode.component.scss'],
+  templateUrl: './active-tasks-user-mode.component.html',
 })
 export class ActiveTasksUserModeComponent {
   @Input() readonly project: Project;
