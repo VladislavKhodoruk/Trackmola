c0e1be77dd215e91ba531770fdde6783e71deb6c
Kirill Borisenok <kborisenok@datamola.com>
2022-07-28 15:44:06 +0300

    fix: added overtimes for the activity total cards

--- src/app/pages/activity/store/activity.selectors.ts
+++ src/app/pages/activity/store/activity.selectors.ts
@@ -15,7 +15,7 @@ export const getActivityPeriod = createSelector(
   ({ period }) => period
 );
 
-export const getWeekReportTime = createSelector(
+export const getReportTime = createSelector(
   getTasksTrack,
   getActivityPeriod,
   (taskTracks, period) =>
@@ -26,7 +26,24 @@ export const getWeekReportTime = createSelector(
           taskTrack.date.seconds * 1000 > period.start &&
           taskTrack.date.seconds * 1000 < period.end
       )
-      .reduce((result, item) => (result += item.duration), 0)
+      .reduce(
+        (result, item) => (result += item.duration + item.overtimeDuration),
+        0
+      )
+);
+
+export const getReportOvertime = createSelector(
+  getTasksTrack,
+  getActivityPeriod,
+  (taskTracks, period) =>
+    taskTracks
+      .filter(
+        (taskTrack) =>
+          taskTrack.userId === localStorage.getItem('AuthUserId') &&
+          taskTrack.date.seconds * 1000 > period.start &&
+          taskTrack.date.seconds * 1000 < period.end
+      )
+      .reduce((result, item) => (result += item.overtimeDuration), 0)
 );
 
 export const getActivePeriod = createSelector(
