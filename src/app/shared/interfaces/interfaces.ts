46a218acf1cd2c6f5057c5fe4d26d5b583bfd2d8
Kirill Borisenok <kborisenok@datamola.com>
2022-07-31 11:30:09 +0300

    feature: added working calendar and holidays

--- src/app/shared/interfaces/interfaces.ts
+++ src/app/shared/interfaces/interfaces.ts
@@ -1,5 +1,8 @@
 import { IconifyIcon } from '@iconify/types';
+
 import { Timestamp } from 'firebase/firestore';
+
+import { DayType } from '@shared/enums/enum';
 
 export interface Project {
   id: string;
@@ -119,4 +122,10 @@ export interface UserCard {
   location: string;
   position: string;
   photo: string;
+}
+
+export interface CalendarDay {
+  date: string;
+  dayType: DayType;
+  holidayName?: string;
 }
