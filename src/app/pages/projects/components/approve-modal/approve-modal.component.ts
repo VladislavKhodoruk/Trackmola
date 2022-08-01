467685b5ae19883c86dfac444730d93f847f20f0
mivakhnenko <mivakhnenko@datamola.com>
2022-08-01 19:14:59 +0300

    fix:fix modal

--- src/app/pages/projects/components/approve-modal/approve-modal.component.ts
+++ src/app/pages/projects/components/approve-modal/approve-modal.component.ts
@@ -1,11 +1,7 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-approve-modal',
   styleUrls: ['./approve-modal.component.scss'],
   templateUrl: './approve-modal.component.html',
 })
-export class ApproveModalComponent {
-  // eslint-disable-next-line @typescript-eslint/no-empty-function
-  constructor() {}
-}
+export class ApproveModalComponent {}
--- src/app/pages/projects/components/approve-modal/approve-modal.container.ts
+++ src/app/pages/projects/components/approve-modal/approve-modal.container.ts
@@ -1,9 +1,6 @@
 import { Component } from '@angular/core';
-
 @Component({
   selector: 'app-approve-modal-container',
   template: '<app-approve-modal></app-approve-modal>',
 })
-export class ApproveModalContainer {
-  constructor() {}
-}
+export class ApproveModalContainer {}
