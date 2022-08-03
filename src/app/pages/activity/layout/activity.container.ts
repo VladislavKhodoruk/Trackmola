27b446d99c704fac0048949378c9411079864eda
Vitaly Cheastyakov <vchistiakov@datamola.com>
2022-08-03 13:19:41 +0300

    add activity for all users except CTO

--- src/app/pages/activity/layout/activity.container.ts
+++ src/app/pages/activity/layout/activity.container.ts
@@ -2,12 +2,31 @@ import { Component } from '@angular/core';
 import { Store } from '@ngrx/store';
 
 import { TrackMolaState } from '@store/trackMola.state';
+import { Observable } from 'rxjs';
+import { PeriodType } from '@shared/enums/enum';
+import { getActivePeriod } from '@pages/activity/store/activity.selectors';
+import {
+  changeActivityPeriodSuccess,
+  getWeekReportTime,
+} from '@pages/activity/store/activity.actions';
 
 @Component({
   selector: 'app-activity-container',
-  template: '<app-activity [userType]="userType"></app-activity>',
+  template: `<app-activity
+    [userType]="userType"
+    [period]="period$ | async"
+    (selectPeriod)="changePeriodOnStore($event)"
+  ></app-activity>`,
 })
 export class ActivityContainer {
+  period$: Observable<PeriodType> = this.store$.select(getActivePeriod);
   readonly userType: string = localStorage.getItem('AuthUserType');
-  constructor(private store$: Store<TrackMolaState>) {}
+
+  constructor(private store$: Store<TrackMolaState>) {
+    this.store$.dispatch(getWeekReportTime());
+  }
+
+  changePeriodOnStore(period: PeriodType) {
+    this.store$.dispatch(changeActivityPeriodSuccess({ choosePeriod: period }));
+  }
 }
