283fdd7d8ef100b058c815864647feb27e899404
Kirill Borisenok <kborisenok@datamola.com>
2022-08-08 13:48:15 +0300

    add-CTO-pages

--- src/app/pages/projects/layout/projects.component.ts
+++ src/app/pages/projects/layout/projects.component.ts
@@ -25,7 +25,10 @@ export class ProjectsComponent {
   readonly checksIcon = checksIcon;
   readonly messagePlusIcon = messagePlus;
 
-  readonly toggleLabels = [ProjectMode.Tasks, ProjectMode.Users];
+  readonly toggleLabels =
+    localStorage.getItem('AuthUserType') !== UserType.CTO
+      ? [ProjectMode.Tasks, ProjectMode.Users]
+      : [ProjectMode.Tasks, ProjectMode.Users, 'by budget'];
 
   readonly userType = UserType;
   readonly currentUser: string = localStorage.getItem('AuthUserType');
